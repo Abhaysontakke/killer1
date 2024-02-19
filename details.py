b47d286320602498ae863e3fe8015e6df4323b25
vspji <155125763+vspji@users.noreply.github.com>
2024-02-19 18:59:46 +0530

    file folder

---
+++ details.py
@@ -0,0 +1,7 @@
+api_id = 22924674
+api_hash = "4490358fa3f47ba78643e02d14018083"
+bot_token = "6747725425:AAH4lL15U5qMpZITO4_TaanVl9pI3tlOorg"
+auth_users = [6804421130]
+sudo_user = -1002041893808
+log_channel = -1001529584600
+txt_channel = -1001529584600
