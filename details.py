057fb3335d262beaa2c5d2492ba052aeb5818acf
vspji <155125763+vspji@users.noreply.github.com>
2024-02-20 08:35:21 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,6 +1,6 @@
 api_id = 22924674
 api_hash = "4490358fa3f47ba78643e02d14018083"
-bot_token = "6495799571:AAHztA-OCqzw-juMQl-yaj1aGq0dIU2RAk0"
+bot_token = "6747725425:AAH4lL15U5qMpZITO4_TaanVl9pI3tlOorg"
 auth_users = [6804421130]
 sudo_user = -1002041893808
 log_channel = -1001529584600
