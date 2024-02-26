41c6f18565cf6586ab24fc3083fcdb2d2577c807
Abhaysontakke <153503226+Abhaysontakke@users.noreply.github.com>
2024-02-26 12:05:19 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
-api_id = 22924674
-api_hash = "4490358fa3f47ba78643e02d14018083"
-bot_token = "6747725425:AAH4lL15U5qMpZITO4_TaanVl9pI3tlOorg"
-auth_users = [6804421130,6748451207]
+api_id = 25791987
+api_hash = "5e2095886ba9bf8187bf56753e922adc"
+bot_token = "6749932097:AAE8M6HUF-pmDRd6WO2seFp21iWisjT9RwI"
+auth_users = [6804421130,6748451207,6986305640,5721835845]
 sudo_user = -1002041893808
-log_channel = -1001529584600
-txt_channel = -1001529584600
+log_channel = -1002050250893
+txt_channel = -1002050250893
