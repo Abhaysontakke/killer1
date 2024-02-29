239b8f7f5d1023006820ea31414e8da524f33fa6
Abhaysontakke <153503226+Abhaysontakke@users.noreply.github.com>
2024-02-29 10:15:17 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -120,6 +120,6 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▰" * completed_length + "▱" * remaining_length
             
             try:
-                await reply.edit(f'\n ╭──⌯════𝗕𝗢𝗧 𝗦𝗧𝗔𝗧𝗜𝗖𝗦═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size ➠ ETA » {tot} - {eta} \n╰─═══✪ @vsp_official™ ✪═══─╯\n') 
+                await reply.edit(f'\n ╭──⌯════𝗕𝗢𝗧 𝗦𝗧𝗔𝗧𝗜𝗖𝗦═════⌯──╮ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size ➠ ETA » {tot} - {eta} \n╰─═══✪ @Killlleeerr ✪═══─╯\n') 
             except FloodWait as e:
                 time.sleep(e.x)
